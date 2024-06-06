42ec8d8497ad46e77f46d3d702a256bf2994df93
Jowin Yip <jowin.yip@gmail.com>
2024-06-06 10:37:26 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,493 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const CHECK_INTERVAL = 24 * 60 * 60 * 1000;
+
+const GUILD_ID = process.env.GUILD_ID || '';
+const CHANNEL_ID = process.env.CHANNEL_ID || '';
+
+const GAMES_FILE_PATH = path.join(__dirname, 'games.json');
+const SETTING_FILE_PATH = path.join(__dirname, 'setting.json');
+
+function loadGames() {
+  try {
+    const data = fs.readFileSync(GAMES_FILE_PATH);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error('Error reading games file:', error);
+    return [];
+  }
+}
+
+function saveGames(games) {
+  try {
+    fs.writeFileSync(GAMES_FILE_PATH, JSON.stringify(games, null, 2));
+  } catch (error) {
+    console.error('Error writing games file:', error);
+  }
+}
+
+function updateMessageId(messageId) {
+  try {
+    fs.writeFileSync(SETTING_FILE_PATH, JSON.stringify({ messageId }, null, 2));
+    updateOriginalMessage(messageId);
+    return true;
+  } catch (error) {
+    console.error('Error writing message id file:', error);
+    return false;
+  }
+}
+
+function checkMessageId() {
+  try {
+    const data = fs.readFileSync(SETTING_FILE_PATH);
+    const messageId = JSON.parse(data).messageId;
+    return messageId ? messageId : false;
+  } catch (error) {
+    console.error('Error reading message id file:', error);
+    return false;
+  }
+}
+
+async function checkForDiscounts() {
+  console.log('Checking for discounts...');
+  const data = loadGames();
+  const decidedList = data.find(list => list.name === "decided");
+  const consideringList = data.find(list => list.name === "considering");
+  const boughtList = data.find(list => list.name === "bought");
+
+  boughtList.list.forEach(async (item) => {
+    try {
+      const response = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${item.id}`);
+      const gameData = response.data[item.id].data;
+      if (gameData) {
+        const discountPercent = gameData.price_overview?.discount_percent || 0;
+        const name = gameData.name;
+        const coming_soon = gameData.release_date?.coming_soon || false;
+        const early_access = gameData.genres?.filter(genre => genre.id === "70").length > 0 || false;
+        const url = `https://store.steampowered.com/app/${item.id}/`;
+        const currentPrice = gameData.price_overview?.final || 0;
+        const currency = gameData.price_overview?.currency || 'undefined';
+        item.name = gameData.name;
+        item.discountPercent = discountPercent;
+        item.url = url;
+        item.coming_soon = coming_soon;
+        item.early_access = early_access;
+      }
+    } catch (error) {
+      console.error(`Error checking for discount on game ID ${item.id}:`, error);
+    }
+  });
+
+  decidedList.list.forEach(async (item) => {
+    try {
+      const response = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${item.id}`);
+      const gameData = response.data[item.id].data;
+      if (gameData) {
+        const discountPercent = gameData.price_overview?.discount_percent || 0;
+        const name = gameData.name;
+        const coming_soon = gameData.release_date?.coming_soon || false;
+        const early_access = gameData.genres?.filter(genre => genre.id === "70").length > 0 || false;
+        const url = `https://store.steampowered.com/app/${item.id}/`;
+        const currentPrice = gameData.price_overview?.final || 0;
+        const currency = gameData.price_overview?.currency || 'undefined';
+        item.name = gameData.name;
+        item.discountPercent = discountPercent;
+        item.url = url;
+        item.coming_soon = coming_soon;
+        item.early_access = early_access;
+
+        if (discountPercent > 0 && item.discountPercent !== discountPercent) {
+          const discountMessage = `The game ${name} is now on sale with a ${discountPercent}% discount! Current price: ${currentPrice / 100} ${currency}.`;
+          const guild = client.guilds.cache.get(GUILD_ID);
+          if (guild) {
+            const channel = guild.channels.cache.get(CHANNEL_ID);
+            if (channel) {
+              channel.send(discountMessage);
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.error(`Error checking for discount on game ID ${item.id}:`, error);
+    }
+  });
+
+  consideringList.list.forEach(async (item) => {
+    try {
+      const response = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${item.id}`);
+      const gameData = response.data[item.id].data;
+      if (gameData) {
+        const discountPercent = gameData.price_overview?.discount_percent || 0;
+        const name = gameData.name;
+        const coming_soon = gameData.release_date?.coming_soon || false;
+        const early_access = gameData.genres?.filter(genre => genre.id === "70").length > 0 || false;
+        const url = `https://store.steampowered.com/app/${item.id}/`;
+        const currentPrice = gameData.price_overview?.final || 0;
+        const currency = gameData.price_overview?.currency || 'undefined';
+        item.name = gameData.name;
+        item.discountPercent = discountPercent;
+        item.url = url;
+        item.coming_soon = coming_soon;
+        item.early_access = early_access;
+
+        if (discountPercent > 0 && item.discountPercent !== discountPercent) {
+          const discountMessage = `The game ${name} is now on sale with a ${discountPercent}% discount! Current price: ${currentPrice / 100} ${currency}.`;
+          const guild = client.guilds.cache.get(GUILD_ID);
+          if (guild) {
+            const channel = guild.channels.cache.get(CHANNEL_ID);
+            if (channel) {
+              channel.send(discountMessage);
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.error(`Error checking for discount on game ID ${item.id}:`, error);
+    }
+  });
+
+  saveGames(data);
+  const messageId = checkMessageId();
+  if (messageId) {
+    updateOriginalMessage(messageId);
+  }
+}
+
+setInterval(checkForDiscounts, CHECK_INTERVAL);
+
+async function addGame(type, gameId, description) {
+  if (isNaN(gameId)) {
+    gameId = gameId.split('/')[4];
+  }
+  const data = loadGames();
+  const decidedList = data.find(list => list.name === "decided");
+  const consideringList = data.find(list => list.name === "considering");
+  const boughtList = data.find(list => list.name === "bought");
+  const allGames = data.reduce((accumulator, currentItem) => {
+    return accumulator.concat(currentItem.list);
+  }, []);
+  if (!allGames.find(game => game.id === gameId)) {
+    try {
+      const response = await axios.get(`https://store.steampowered.com/api/appdetails?appids=${gameId}`);
+      const gameData = response.data[gameId].data;
+      if (gameData) {
+        const discountPercent = gameData.price_overview?.discount_percent || 0;
+        const name = gameData.name;
+        const coming_soon = gameData.release_date?.coming_soon || false;
+        const early_access = gameData.genres?.filter(genre => genre.id === "70").length > 0 || false;
+        const url = `https://store.steampowered.com/app/${gameId}/`;
+        const currentPrice = gameData.price_overview?.final || 0;
+        const currency = gameData.price_overview?.currency || 'undefined';
+        if (type === "decided") {
+          decidedList.list.push({ id: gameId, name, discountPercent, url, coming_soon, early_access, description });
+        } else if (type === "considering") {
+          consideringList.list.push({ id: gameId, name, discountPercent, url, coming_soon, early_access, description });
+        } else if (type === "bought") {
+          boughtList.list.push({ id: gameId, name, discountPercent, url, coming_soon, early_access, description });
+        }
+
+        if (discountPercent > 0) {
+          const discountMessage = `The game ${name} is now on sale with a ${discountPercent}% discount! Current price: ${currentPrice / 100} ${currency}.`;
+          const guild = client.guilds.cache.get(GUILD_ID);
+          if (guild) {
+            const channel = guild.channels.cache.get(CHANNEL_ID);
+            if (channel) {
+              channel.send(discountMessage);
+            }
+          }
+        }
+
+        saveGames(data);
+        const messageId = checkMessageId();
+        if (messageId) {
+          updateOriginalMessage(messageId);
+        }
+
+        return JSON.stringify({ status: 'success', messageText: 'Game added' });
+      } else {
+        return JSON.stringify({ status: 'error', messageText: 'Invalid game url or game id.' });
+      }
+
+    } catch (error) {
+      console.error(`Error adding game with ID ${gameId}:`, error);
+      return JSON.stringify({ status: 'error', messageText: 'Unable to call Steam API.' });
+    }
+  } else {
+    let messageText = 'Game already in the list';
+    let gameIndex = boughtList.list.findIndex(game => game.id === gameId);
+    if (gameIndex !== -1 && type !== "bought") {
+      const [game] = boughtList.list.splice(gameIndex, 1);
+      if (description != '') {
+        game.description = description;
+      }
+      if (type === "decided") {
+        decidedList.list.push(game);
+        messageText = 'Game transferred from bought to decided';
+      } else if (type === "considering") {
+        consideringList.list.push(game);
+        messageText = 'Game transferred from bought to considering';
+      }
+    } else if (type === "bought") {
+      if (description != '') {
+        boughtList.list[gameIndex].description = description;
+      }
+    }
+
+    gameIndex = consideringList.list.findIndex(game => game.id === gameId);
+    if (gameIndex !== -1 && type !== "considering") {
+      const [game] = consideringList.list.splice(gameIndex, 1);
+      if (description != '') {
+        game.description = description;
+      }
+      if (type === "decided") {
+        decidedList.list.push(game);
+        messageText = 'Game transferred from considering to decided';
+      } else if (type === "bought") {
+        boughtList.list.push(game);
+        messageText = 'Game transferred from considering to bought';
+      }
+    } else if (type === "considering") {
+      if (description != '') {
+        consideringList.list[gameIndex].description = description;
+      }
+    }
+
+    gameIndex = decidedList.list.findIndex(game => game.id === gameId);
+    if (gameIndex !== -1 && type !== "decided") {
+      const [game] = decidedList.list.splice(gameIndex, 1);
+      if (description != '') {
+        game.description = description;
+      }
+      if (type === "considering") {
+        consideringList.list.push(game);
+        messageText = 'Game transferred from decided to considering';
+      } else if (type === "bought") {
+        boughtList.list.push(game);
+        messageText = 'Game transferred from decided to bought';
+      }
+    } else if (type === "decided") {
+      if (description != '') {
+        decidedList.list[gameIndex].description = description;
+      }
+    }
+
+    saveGames(data);
+    const messageId = checkMessageId();
+    if (messageId) {
+      updateOriginalMessage(messageId);
+    }
+    return JSON.stringify({ status: 'success', messageText });
+  }
+}
+
+async function removeGame(gameId) {
+  if (isNaN(gameId)) {
+    gameId = gameId.split('/')[4];
+  }
+  const data = loadGames();
+  const decidedList = data.find(list => list.name === "decided");
+  const consideringList = data.find(list => list.name === "considering");
+  const boughtList = data.find(list => list.name === "bought");
+  let gameIndex = boughtList.list.findIndex(game => game.id === gameId);
+  if (gameIndex !== -1) {
+    boughtList.list.splice(gameIndex, 1);
+    saveGames(data);
+    const messageId = checkMessageId();
+    if (messageId) {
+      updateOriginalMessage(messageId);
+    }
+    return JSON.stringify({ status: 'success', messageText: 'Game removed from bought list' });
+  }
+
+  gameIndex = consideringList.list.findIndex(game => game.id === gameId);
+  if (gameIndex !== -1) {
+    consideringList.list.splice(gameIndex, 1);
+    saveGames(data);
+    const messageId = checkMessageId();
+    if (messageId) {
+      updateOriginalMessage(messageId);
+    }
+    return JSON.stringify({ status: 'success', messageText: 'Game removed from considering list' });
+  }
+
+  gameIndex = decidedList.list.findIndex(game => game.id === gameId);
+  if (gameIndex !== -1) {
+    decidedList.list.splice(gameIndex, 1);
+    saveGames(data);
+    const messageId = checkMessageId();
+    if (messageId) {
+      updateOriginalMessage(messageId);
+    }
+    return JSON.stringify({ status: 'success', messageText: 'Game removed from decided list' });
+  }
+  return JSON.stringify({ status: 'error', messageText: 'Game not found in the list.' });
+}
+
+async function updateOriginalMessage(messageId) {
+  const data = loadGames();
+  const decidedList = data.find(list => list.name === "decided");
+  const consideringList = data.find(list => list.name === "considering");
+  const boughtList = data.find(list => list.name === "bought");
+
+  let discountNow = '\*\*==DISCOUNT NOW==\*\*\n';
+  let bought = '\*\*==BOUGHT==\*\*\n';
+  let decided = '\*\*==DECIDED==\*\*\n';
+  let considering = '\*\*==CONSIDERING==\*\*\n';
+
+  boughtList.list.sort((a, b) => {
+    if (a.coming_soon && !b.coming_soon) return 1;
+    if (!a.coming_soon && b.coming_soon) return -1;
+    if (a.early_access && !b.early_access) return 1;
+    if (!a.early_access && b.early_access) return -1;
+    return 0;
+  });
+
+  boughtList.list.forEach(item => {
+    bought += `${item.name} ${item.description ? item.description : ''}${item.coming_soon ? ` \`Coming Soon\`` : ''}${item.early_access ? ` \`Early Access\`` : ''}\n${item.url}\n`;
+  });
+
+  decidedList.list.sort((a, b) => {
+    if (a.coming_soon && !b.coming_soon) return 1;
+    if (!a.coming_soon && b.coming_soon) return -1;
+    if (a.early_access && !b.early_access) return 1;
+    if (!a.early_access && b.early_access) return -1;
+    return 0;
+  });
+
+  decidedList.list.forEach(item => {
+    if (item.discountPercent > 0) {
+      discountNow += `${item.name} ${item.discountPercent}% OFF \n${item.url}\n`;
+    }
+    decided += `${item.name} ${item.description ? item.description : ''}${item.coming_soon ? ` \`Coming Soon\`` : ''}${item.early_access ? ` \`Early Access\`` : ''}\n${item.url}\n`;
+  });
+
+  consideringList.list.sort((a, b) => {
+    if (a.coming_soon && !b.coming_soon) return 1;
+    if (!a.coming_soon && b.coming_soon) return -1;
+    if (a.early_access && !b.early_access) return 1;
+    if (!a.early_access && b.early_access) return -1;
+    return 0;
+  });
+
+  consideringList.list.forEach(item => {
+    if (item.discountPercent > 0) {
+      discountNow += `${item.name} ${item.discountPercent}% OFF \n${item.url}\n`;
+    }
+    considering += `${item.name} ${item.description ? item.description : ''}${item.coming_soon ? ` \`Coming Soon\`` : ''}${item.early_access ? ` \`Early Access\`` : ''}\n${item.url}\n`;
+  });
+
+  const guild = client.guilds.cache.get(GUILD_ID);
+  if (guild) {
+    const channel = guild.channels.cache.get(CHANNEL_ID);
+    if (channel) {
+      const message = await channel.messages.fetch(messageId);
+      if (message) {
+        message.edit(`${discountNow}\n${bought}\n${decided}\n${considering}`);
+      }
+    }
+  }
+}
+
+client.login(BOT_TOKEN);
+
+client.once('ready', () => {
+  console.log('Ready to check for discounts!');
+  checkForDiscounts();
+});
+
+client.on('messageCreate', async message => {
+  const sendHelpMessage = () => {
+    const helpText = `Available commands:\n\n` +
+      `!addBought <game_id> <message (optional)> - Add a game to the bought list.\n` +
+      `!addDecided <game_id> <message (optional)> - Add a game to the decided list.\n` +
+      `!addConsidering <game_id> <message (optional)> - Add a game to the considering list.\n` +
+      `!removegame <game_id> - Remove a game from the watch list.\n` +
+      `!listgames - List all games being watched.\n` +
+      `!updateMessageId <message_id> - Message that hold latest information\n` +
+      `!checkdiscounts - Check for discounts on all watched games.`;
+    message.channel.send(helpText);
+  };
+
+  const handleAddGame = async (type) => {
+    const [command, gameId, ...descriptionParts] = message.content.split(' ');
+    const description = descriptionParts.join(' ');
+    const { status, messageText } = JSON.parse(await addGame(type, gameId, description));
+
+    if (status === 'success') {
+      const messageId = checkMessageId();
+      const successMessage = messageId
+        ? `${messageText}. Check pinned message for the list of games being watched.`
+        : `Game added to the watch list. Please set the message id using !updateMessageId <message_id> command to get updates in a pinned message.`;
+      message.channel.send(successMessage);
+    } else {
+      message.channel.send(`${messageText}.`);
+    }
+  };
+
+  const handleRemoveGame = async () => {
+    const [command, gameId] = message.content.split(' ');
+    const { status, messageText } = JSON.parse(await removeGame(gameId));
+
+    if (status === 'success') {
+      const messageId = checkMessageId();
+      const successMessage = messageId
+        ? `${messageText}. Check pinned message for the list of games being watched.`
+        : `Game removed from the watch list. Please set the message id using !updateMessageId <message_id> command to get updates in a pinned message.`;
+      message.channel.send(successMessage);
+    } else {
+      message.channel.send(`${messageText}.`);
+    }
+  };
+
+  const handleListGames = () => {
+    const isMessageIdSet = checkMessageId();
+    const listMessage = isMessageIdSet
+      ? `Please check pinned message for the list of games being watched. If you wish to change message id, please use !updateMessageId <message_id> command.`
+      : 'Looks like the message ID is not set. Get the message id and please set it using !updateMessageId <message_id> command. Remember to pin the message.';
+    message.channel.send(listMessage);
+  };
+
+  const handleUpdateMessageId = () => {
+    const [command, messageId] = message.content.split(' ');
+    const updateSuccess = updateMessageId(messageId);
+
+    if (updateSuccess) {
+      message.channel.send(`Message ID updated to ${messageId}. Future updates will be posted in this message.`);
+    } else {
+      message.channel.send('Error updating message ID. Please try again.');
+    }
+  };
+
+  const handleCheckDiscounts = () => {
+    checkForDiscounts();
+    const messageId = checkMessageId();
+    if (messageId) {
+      message.channel.send('Game list updated. Check pinned message for the list of games being watched.');
+    }
+  };
+
+  if (message.content === '!help') {
+    sendHelpMessage();
+  } else if (message.content.startsWith('!addBought ')) {
+    handleAddGame('bought');
+  } else if (message.content.startsWith('!addDecided ')) {
+    handleAddGame('decided');
+  } else if (message.content.startsWith('!addConsidering ')) {
+    handleAddGame('considering');
+  } else if (message.content.startsWith('!removegame ')) {
+    handleRemoveGame();
+  } else if (message.content === '!listgames') {
+    handleListGames();
+  } else if (message.content.startsWith('!updateMessageId')) {
+    handleUpdateMessageId();
+  } else if (message.content === '!checkdiscounts') {
+    handleCheckDiscounts();
+  }
+});
